deefa016e989836daa3f5f4d21bbf2c803cd8550
Svetlin Ralchev <svet@ralch.com>
2018-12-19 16:47:50 +0100

    Add aliases

--- responder.go
+++ responder.go
@@ -15,6 +15,11 @@ import (
 )
 
 var (
+	// Status sets a HTTP response status code hint into request context at any point
+	// during the request life-cycle. Before the Responder sends its response header
+	// it will check the StatusCtxKey
+	Status = render.Status
+
 	// Respond is a package-level variable set to our default Responder. We do this
 	// because it allows you to set render.Respond to another function with the
 	// same function signature, while also utilizing the render.Responder() function
@@ -29,10 +34,19 @@ var (
 	// RenderList renders a slice of payloads and responds to the client request.
 	RenderList = render.RenderList
 
-	// Status sets a HTTP response status code hint into request context at any point
-	// during the request life-cycle. Before the Responder sends its response header
-	// it will check the StatusCtxKey
-	Status = render.Status
+	// PlainText writes a string to the response, setting the Content-Type as
+	// text/plain.
+	PlainText = render.PlainText
+
+	// Data writes raw bytes to the response, setting the Content-Type as
+	// application/octet-stream.
+	Data = render.Data
+
+	// HTML writes a string to the response, setting the Content-Type as text/html.
+	HTML = render.HTML
+
+	// NoContent returns a HTTP 204 "No Content" response.
+	NoContent = render.NoContent
 )
 
 // StatusErr returns the status code for given error
@@ -80,6 +94,26 @@ func WrapError(err error) *errorx.Errorx {
 // DefaultResponder handles streaming JSON and XML responses, automatically setting the
 // Content-Type based on request headers. It will default to a JSON response.
 func DefaultResponder(w http.ResponseWriter, r *http.Request, v interface{}) {
+	v = response(r, v)
+	render.DefaultResponder(w, r, v)
+}
+
+// JSON marshals 'v' to JSON, automatically escaping HTML and setting the
+// Content-Type as application/json.
+func JSON(w http.ResponseWriter, r *http.Request, v interface{}) {
+	v = response(r, v)
+	render.JSON(w, r, v)
+}
+
+// XML marshals 'v' to JSON, setting the Content-Type as application/xml. It
+// will automatically prepend a generic XML header (see encoding/xml.Header) if
+// one is not found in the first 100 bytes of 'v'.
+func XML(w http.ResponseWriter, r *http.Request, v interface{}) {
+	v = response(r, v)
+	render.XML(w, r, v)
+}
+
+func response(r *http.Request, v interface{}) interface{} {
 	if err, ok := v.(error); ok {
 		errx, ok := err.(*errorx.Errorx)
 
@@ -107,8 +141,8 @@ func DefaultResponder(w http.ResponseWriter, r *http.Request, v interface{}) {
 			logger.Panic(err, debug.Stack())
 		}
 
-		v = errx
+		return errx
 	}
 
-	render.DefaultResponder(w, r, v)
+	return v
 }
